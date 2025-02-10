001c5b622ae0013bccf02358245df4c9688e10b9
evez <evezhuseynov24@gmail.com>
2025-02-11 00:51:33 +0400

    Optimization "Valid Anagram" solution. New approach.

--- src/main/java/com/huseynov/easy/ValidAnagram_242.java
+++ src/main/java/com/huseynov/easy/ValidAnagram_242.java
@@ -7,7 +7,37 @@ public class ValidAnagram_242 {
 //        String s = "anagram", t = "nagaram";
         String s = "rat", t = "car";
 
-        System.out.println("chekc1: " + isAnagram1(s, t));
+        System.out.println("chekc1: " + isAnagram2(s, t));
+    }
+
+    public static boolean isAnagram2(String s, String t) {
+        // if their sizes are different
+        if (s.length() != t.length()) return false;
+
+        // if s and t is anagram, then their different character count equal.
+        // Exp: if count of "a" in s equal 5, then count of "a" in t must equal is 5
+        // I count each character and to increase suitable index in array
+        // "a"'s index equal is 0, b equal is 1 , ....
+        // s and t consist of lowercase English letters. and count of different character is 27
+        int[] countOfChar = new int[27];
+        for (int i = 0; i < s.length(); i++) {
+            int indexOfASCII = s.charAt(i); // a=>97,b=>98
+            countOfChar[indexOfASCII - 97]++; // count of "a" in s = countOfChar[0], b=countOfChar[1]   ...;    a=0,b=1 ...
+        }
+        // if , I find "a" in t, then I must decrease to 1 unit,"a" suitable index in array
+        for (int i = 0; i < t.length(); i++) {
+            int indexOfASCII = t.charAt(i);
+            countOfChar[indexOfASCII - 97]--;
+        }
+
+        //if s and t is anagram, then all countOfChar's value equal must zero;
+        for (int count : countOfChar) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static boolean isAnagram1(String s, String t) {
@@ -41,5 +71,40 @@ public class ValidAnagram_242 {
 //        System.out.println(tHashMap);
         return sHashMap.equals(tHashMap);
     }
+
+    public static boolean isAnagram3(String s, String t) {
+        // if their sizes are different
+        if (s.length() != t.length()) return false;
+
+        // if s and t is anagram, then their different character count equal.
+        // Exp: if count of "a" in s equal 5, then count of "a" in t must equal is 5
+        // I count each character and to increase suitable index in array
+        // "a"'s index equal is 0, b equal is 1 , ....
+        // s and t consist of lowercase English letters. and count of different character is 27
+        int[] countOfChar = new int[27];
+        for (int i = 0; i < s.length(); i++) { // already s.length() = t.length()
+            int indexOfASCIIForS = s.charAt(i); // a=>97,b=>98
+            int indexOfASCIIForT = t.charAt(i); // a=>97,b=>98
+            // count of "a" in s = countOfChar[0], b=countOfChar[1]   ...;    a=0,b=1 ...
+            countOfChar[indexOfASCIIForS - 97]++;
+            countOfChar[indexOfASCIIForT - 97]--;
+        }
+
+        // if , I find "a" in t, then I must decrease to 1 unit,"a" suitable index in array
+        // for (int i = 0; i < t.length(); i++) {
+        // int indexOfASCII = t.charAt(i);
+        // countOfChar[indexOfASCII - 97]--;
+        // }
+
+        //if s and t is anagram, then all countOfChar's value equal must zero;
+        for (int count : countOfChar) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 
 }
