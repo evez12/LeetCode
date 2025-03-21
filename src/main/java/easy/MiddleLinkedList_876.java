16f8aea317d1c7ceb565590970468dcde3b3c363
evez <evezhuseynov24@gmail.com>
2025-03-21 15:34:23 +0400

    added new problem "Middle Linked List". (LeetCode 876)

---
+++ src/main/java/easy/MiddleLinkedList_876.java
@@ -0,0 +1,29 @@
+package easy;
+
+public class MiddleLinkedList_876 {
+    public static void main(String[] args) {
+        ListNode middle1 = middleNode(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))));
+        ListNode middle2 = middleNode(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4)))));
+        ListNode middle3 = middleNode(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6)))))));
+        System.out.println("Middle 1: " + middle1.val);
+        System.out.println("Middle 2: " + middle2.val);
+        System.out.println("Middle 3: " + middle3.val);
+    }
+
+    //        /*
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null) {
+            fast = fast.next;
+            if (fast != null) {
+                fast = fast.next;
+            }
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+
+//     */
+}
